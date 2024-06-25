1bf7a8cd1c7dc62d6d62193d79ee85916ab129e9
Irfan <irfan188iqbal@gmail.com>
2024-06-25 09:39:29 +0530

    update the ui

--- src/Components/wishlist.jsx
+++ src/Components/wishlist.jsx
@@ -4,14 +4,14 @@ import Navbar2 from './Navbar2';
 import { MenuContext } from '../MenuContext';
 
 const Wishlist = () => {
-    const { favorites,removeFromFavorites } = useContext(MenuContext);
+    const { favorites, removeFromFavorites } = useContext(MenuContext);
     const [favorite, setFavorites] = useState([]);
 
     useEffect(() => {
         setFavorites(favorites);
-      }, [favorite]);
-    
- 
+    }, [favorite]);
+
+
     const clearAllFavorites = () => {
         setFavorites([]);
         localStorage.removeItem('favorite');
@@ -22,10 +22,10 @@ const Wishlist = () => {
             <Navbar2 />
             <div className="mx-auto p-4">
                 <div className="flex justify-between items-center mb-4">
-                    <h1 className="text-2xl font-bold">My Wishlist</h1>
+                    <h1 className="text-2xl font-bold"> Wishlist</h1>
                     {favorites.length > 0 && (
                         <button
-                            className="bg-red-500 text-white px-4 py-2 rounded-md"
+                            className="border border-black text-black px-4 py-2 rounded-full"
                             onClick={clearAllFavorites}
                         >
                             Clear All
@@ -45,14 +45,14 @@ const Wishlist = () => {
                                 <p className="text-md text-gray-700 mb-2 ">{item?.description}</p>
                                 <p className="text-lg font-bold text-gray-900 mb-4">₹{item.price}</p>
                                 <button
-                                    className="absolute top-2 right-2 bg-red-500 text-white p-1 rounded-full"
+                                    className="absolute top-2 right-2 bg-gray-400 text-white p-1 rounded-full"
                                     onClick={() => removeFromFavorites(item)}
                                     title="Remove from Wishlist"
                                 >
                                     <svg
                                         className="w-5 h-5"
                                         fill="none"
-                                        stroke="currentColor"
+                                        stroke="white"
                                         viewBox="0 0 24 24"
                                         xmlns="http://www.w3.org/2000/svg"
                                     >
@@ -63,15 +63,21 @@ const Wishlist = () => {
                                             d="M6 18L18 6M6 6l12 12"
                                         ></path>
                                     </svg>
+
                                 </button>
                                 <div className='my-2'>
-                          <Link to={`/detailPage`} state={{item}} className='border border-current px-3 py-2 my-2 rounded-md text-sm ml-2'>View</Link>
-                            </div>
+                                    <Link to={`/detailPage`} state={{ item }} className='border border-current px-3 py-2 my-2 rounded-md text-sm ml-2'>View</Link>
+                                </div>
                             </div>
                         ))}
                     </div>
                 ) : (
-                    <p className="text-center text-gray-700">Your wishlist is empty.</p>
+                    <>
+                        <p className="text-center text-gray-700 font-bold">Your wishlist is empty.</p>
+                        <div>
+                            <img className='animate-pulse' src={"https://res.cloudinary.com/dlcnf8yfh/image/upload/v1719287495/Firefly_acartoon_charectors_pick_burger_and_juice_to_trolly_87268-removebg-preview_eaapj8.png"} alt="" />
+                        </div>
+                    </>
                 )}
             </div>
         </>
